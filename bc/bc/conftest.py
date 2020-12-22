3df2af568a14524550701c315cf071457aa2eb0e
Tony Perez <tonper19@gmail.com>
2020-12-22 17:55:58 +0200

    0001. Generated from https://github.com/pydanny/cookiecutter-django project template

---
+++ bc/bc/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bc.users.models import User
+from bc.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
